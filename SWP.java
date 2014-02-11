6495c8215d051f375611c992cc5c0a1ccd10189e
Zhang Danyang <zhangdanyangg@gmail.com>
2014-02-11 19:38:39 +0800

    finished event handling

--- SWP.java
+++ SWP.java
@@ -10,6 +10,12 @@
  *          Nanyang Technological University                     *
  *          Singapore 639798                                     *
  *===============================================================*/
+/**
+ * User: Danyang
+ * Date: 2/11/14
+ * Time: 6:35 PM
+ */
+
 public class SWP {
 
     /*========================================================================*
@@ -80,14 +86,14 @@ public class SWP {
         implement your Protocol Variables and Methods below:
      *==========================================================================*/
     private boolean no_nak = true;
-    private Packet int_buf[] = new Packet[NR_BUFS];
+    private Packet in_buf[] = new Packet[NR_BUFS];
 
     /**
      * Initilize inf_buf[]
      */
-    private void init_int_buff() {
+    private void init_in_buff() {
         for (int i=0; i<NR_BUFS; i++) {
-            this.int_buf[i] = new Packet();
+            this.in_buf[i] = new Packet();
         }
     }
 
@@ -145,7 +151,7 @@ public class SWP {
      */
     public void protocol6() {
         this.init(); // initialize the out buffer
-        this.init_int_buff(); // initialize the input buffer
+        this.init_in_buff(); // initialize the input buffer
         this.init_arrived(); // initialize the arrived array
         // Send
         // e.g. 0 1 2 | 3 4 5 6 | 7 8
@@ -157,42 +163,78 @@ public class SWP {
 
         int i; // index into buffer array
         PFrame frame_received = new PFrame();  // Scratch
-        int nbuffered = 0; // how many output buffers currently used
+        int nr_output_buffered = 0; // how many output buffers currently used
 
         this.enable_network_layer(NR_BUFS);
         while(true) {
             wait_for_event(event);
             switch(event.type) {
-                case (PEvent.NETWORK_LAYER_READY):
-                    nbuffered++;
+                case (PEvent.NETWORK_LAYER_READY): // sending out
+                    nr_output_buffered++;
                     this.from_network_layer(out_buf[next_frame_to_send%NR_BUFS]);
-                    this.send_frame(PFrame.DATA, next_frame_to_send, frame_expected, out_buf);
+                    this.send_frame(PFrame.DATA, next_frame_to_send, frame_expected, this.out_buf);
                     next_frame_to_send++;
                     break;
-                case (PEvent.FRAME_ARRIVAL ):
+                case (PEvent.FRAME_ARRIVAL ): // receiving
                     this.from_physical_layer(frame_received);
                     if(frame_received.kind==PFrame.DATA) {
                         // An undamanged frame has arrived
                         if(frame_received.seq!=frame_expected&&this.no_nak) {
-                            this.send_frame(PFrame.NAK, 0, frame_expected, out_buf);
+                            this.send_frame(PFrame.NAK, 0, frame_expected, this.out_buf); // seq number not expected
                         }
                         else {
                             this.start_ack_timer();
                         }
-                        // TODO 
 
+                        if(this.between(frame_expected, frame_received.seq, too_far)&&!arrived[frame_received.seq%NR_BUFS]) {
+                            // frames received may be in any order
+                            this.arrived[frame_received.seq%NR_BUFS] = true;
+                            this.in_buf[frame_received.seq%NR_BUFS] = frame_received.info;
+                            while(arrived[frame_expected%NR_BUFS]){
+                                /*
+                                Pass frames and advance window.
+                                [expected, .., frame_received, .., too far]
+                                 */
+                                this.to_network_layer(this.in_buf[frame_expected%NR_BUFS]);
+                                this.no_nak = true;
+                                this.arrived[frame_expected%NR_BUFS] = false;
+                                frame_expected++;
+                                too_far++;
+                                this.start_ack_timer();
+                            }
+                        }
+                    }
+                    if(frame_received.kind==PFrame.NAK&&this.between(ack_expected, (frame_received.ack+1)%(MAX_SEQ+1), next_frame_to_send)) {
+                        /*
+                        frame_received.ack is all frames received correctly received and acknowledged
+                        nak for the
+                         */
+                        this.send_frame(PFrame.DATA, (frame_received.seq+1)%(MAX_SEQ+1), frame_expected, this.out_buf);
+                    }
+
+                    while(between(ack_expected, frame_received.ack, next_frame_to_send)) {
+                        // acknowledgement received for the [lower, .., ack]
+                        nr_output_buffered--;
+                        stop_timer(ack_expected%NR_BUFS);
+                        ack_expected++;
                     }
                     break;
                 case (PEvent.CKSUM_ERR):
+                    if(this.no_nak) {
+                        this.send_frame(PFrame.NAK, 0, frame_expected, this.out_buf); // damaged frame
+                    }
                     break;
                 case (PEvent.TIMEOUT):
+                    this.send_frame(PFrame.DATA, this.oldest_frame, frame_expected, this.out_buf);
                     break;
                 case (PEvent.ACK_TIMEOUT):
+                    this.send_frame(PFrame.ACK, 0, frame_expected, this.out_buf);
                     break;
                 default:
                     System.out.println("SWP: undefined event type = "  + event.type);
                     System.out.flush();
             }
+            // Enable Disable buffer
         }
     }
 
